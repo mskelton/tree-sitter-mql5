418c54c0634313a2a9a1f27b62caacca993d17b4
Mark Skelton <mdskelton99@gmail.com>
2023-01-06 22:43:12 -0600

    Support `input` storage class

--- grammar.js
+++ grammar.js
@@ -2,5 +2,7 @@ const CPP = require("tree-sitter-cpp/grammar")
 
 module.exports = grammar(CPP, {
   name: "mql5",
-  rules: {},
+  rules: {
+    storage_class_specifier: ($, original) => choice(original, "input"),
+  },
 })
